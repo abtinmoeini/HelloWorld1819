40d2e1793b3fb9d1c993aafc4d763249c755024a
abtin.moeini <abtin.moeini@ams-gym-edv5-05.gym.ams.local>
2018-12-17 16:14:20 +0100

    gute

---
+++ HalloWeltProjekt/src/basics/Bedingung.java
@@ -0,0 +1,55 @@
+package basics;
+
+public class Bedingung {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    boolean bool_var;
+    bool_var = false
+    		//
+    		if (bool_var == false) {
+    			System.out.println("1 - Bool Var ist false");
+	}
+	// achtung!!, nur eine zuweisung, kein vergleich, daher
+    // muss bool_var true sein damit sysout ausgeführt wird
+	if (bool_var = false) {
+		System.out.println("2 - Bool Var ist false");
+	}
+	//
+	if (bool_var) {
+    System.out.println("Bool Var ist wahr");
+	}
+	//
+	//wenn dann sonst
+	if (bool_var) {
+		System.out.println("wahr");
+	} else {
+		System.out.println("falsch");
+	}
+	//
+	// das "&&" verbindet zwei abfragen, die beide wahr sein müssen
+	if (true && true && true) {
+		System.out.println("wahr");
+	}
+	//
+	if /true && false && true) {
+		System.out.println("nix");
+	}
+	//
+	boolean bool1 = true;
+	boolean bool2 = false;
+	boolean bool3 = true:
+	//
+		if (bool1 && bool2 && bool3) {
+			System.out.println("alle 3 bools true");
+		}
+	// "&&" logisches UND
+	// "||" logisches ODER
+	if (bool1 || bool2 || bool3) {
+		System.out.println("einer der 3 bools ist true");
+	}
+
+	}
+    
+    
+}
---
+++ HalloWeltProjekt/src/basics/Schleife.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class Schleife {
+
+	public static void main(String[] args) {
+		// FOR-Schleife
+		// 1. Variable Deklaration & Zuweisung
+		// 2. Die Bedingung
+		// 3. increment / decrement
+		for (int i = 0; i < 10; i=i+1) {
+			System.out.println("Zahl: " + i);
+			
+			
+			
+			
+		}
+	}
+
+}
