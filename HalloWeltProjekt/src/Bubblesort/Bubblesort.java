277db784a61d03c180fd31cff935a4eb7a98f3a4
abtin.moeini <abtin.moeini@AMS-GYM-EDV1-06.gym.ams.local>
2019-02-11 16:15:58 +0100

    bubblesort

---
+++ HalloWeltProjekt/src/Bubblesort/Bubblesort.java
@@ -0,0 +1,29 @@
+package Bubblesort;
+
+public class Bubblesort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] zahlen = { 3, 6, 1, 9, 2, 5 };
+
+		//
+		for (int i = 0; i < zahlen.length; i++) {
+			System.out.println(zahlen[i]);
+		}
+
+		for (int n = zahlen.length; n > 1; n--) {
+			for (int i = 0; i < n-1; i++) {
+				if (zahlen[i] > zahlen[i + 1]) {
+					// A.swap(i, i+1)
+					int help = zahlen[i];
+					zahlen[i] = zahlen[i + 1];
+					zahlen[i + 1] = help;
+				}
+			}
+		}
+		for (int i = 0; i < zahlen.length; i++) {
+			System.out.println(zahlen[i]);
+		}
+	}
+}
