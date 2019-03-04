7e684915ee532050ad341057ee3063c092a777b7
abtin.moeini <abtin.moeini@AMS-GYM-EDV1-06.gym.ams.local>
2019-03-04 16:17:09 +0100

    quicksort

--- HalloWeltProjekt/src/Bubblesort/Quicksort.java
+++ HalloWeltProjekt/src/Bubblesort/Quicksort.java
@@ -1,15 +1,23 @@
 package Bubblesort;
 
 public class Quicksort {
+	static int[] zahlen = { 9, 2, 5, 1, 7, 10, 6, 4, 3 };
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int links = 0;
+		int rechts = zahlen.length - 1;
+		//
+
+		quicksort(0, n - 1);
 
 	}
-public static void quicksort (int links, int rechts) {
-if (links < rechts) {
+
+	public static void quicksort(int links, int rechts) {
+	}
+
+public static int teile(int links, int rechts) {
+int i = links
+int j = rechts - 1
 }
-
-
 
 }
